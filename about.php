0cd576e8c34f57e84a9235e63912cd986b1e3c4f
Colorful Arty <35205060+colorfularty@users.noreply.github.com>
2018-04-03 10:38:38 -0400

    Updated the people on the about page

--- about.php
+++ about.php
@@ -35,27 +35,7 @@
 	   <!--adding about me section - ma-->
 	       <article class="indent">
 	           <h1>About Us</h1>
-	           <section>
-	           <h2>Melissa</h2>
-	           <p>A writing major at Grand Valley State University and an avid reader. Melissa hopes to one day join the ranks of other authors, but in the meantime enjoys reading other's work in her spare time. This website is a place for her to share her thoughts an opinions on the books she reads. </p>
-	           </section>
-	           
-	           <aside class="about">
-	               <img src="images/ma/portrait@2x.jpg" alt="Portrait of Melissa">	 
-	           </aside>
-	           
-	           <!--AS-->
-	           <section>
-	               <h2>Austin Shamoun</h2>
-	               <p>I am and Information Systems Major here at Grand Valley State University with a double minor in Business and Computer Science. I am an avid videogame player as well as someone who enjoys well written TV shows. The TV shows I chose to write about exceeded my expectations on every level. They were well written, filmed, and directed exceptionally. So I hope you have enjoy my reviews on some of my favorite TV shows and hope you will definitely take the time to watch them!</p>
-	           </section>
-	       
-	           <aside>
-	               <img src="images/as/me2x1.jpg " alt="Portrait of Austin Shamoun">
-	           </aside>
-	           
-	           <!-- AD-->
-                <section>
+	            <section>
                 <h2>Andrew DeVries</h2>
                 
                 <p>Information Systems Major at Grand Valley State University with a minor in Business and I.S. Security. I am a huge fan of movies and have a deep interest in the box office and the business side of making movies. One of my favorite places is the theatre and I usualy will always enjoy myself no matter the movie. You'll find just some of my thoughts on some of the biggest movies of today.</p>
@@ -63,15 +43,21 @@
                 <aside>
                     <img src="images/ad/calvin.jpg" alt="profile picture">
                 </aside>
-	       
-	       <!-- SA -->
-	           <section>
-	           <h2>Sanil Apte</h2>
-	           <p>Information Systems Major at Grand Valley State University with a minor in Business. I've been playing video games since I was a child. As I've grown older, my love for them has increased tenfold. I do not proclaim to great or even good at some of the video games I review, but I enjoy writing about them nonetheless. I hope you look at some of my thoughts and they help you make some decisions.</p>
-	           </section>
-	           <aside>
-	               <img src="images/sa/link.jpg" alt="link profile">
-	           </aside>
+	            <section>
+	            <h2>Aaron Teague</h2>
+	            
+	            <p>To be written...</p>
+	            </section>
+	            <aside>
+	            </aside>
+	            <section>
+	            <h2>Josh Techentin</h2>
+	            
+	            <p>Computer Science Major at Grand Valley State University with a minor in Mathematics. I am a senior set to graduate in April 2018. I am extremely passionate about video game design creation, and I hope one day to enter the indie developer scene. As for my hobbies, I love biking in the country, making small video games to test my programming skills, and hanging out with my friends at His House.</p>
+	            </section>
+	            <aside>
+	            	<img src="images/jt/new arty.png" alt="profile picture">
+	            </aside>
 	       </article>
 	       
 	   
